2c683f9bbf3d1392aa9f8c4ae1cd728b7415898b
colin <colinsullivan203@gmail.com>
2023-03-03 15:30:50 -0800

    fixed player-player interaction, added player living to observation space, fixed action space, adjusted default world constants, and changed player color

--- simple_agar/envs/base_world.py
+++ simple_agar/envs/base_world.py
@@ -1,5 +1,5 @@
 import gymnasium as gym
-from gymnasium import spaces
+from gymnasium.spaces import Box, Dict, MultiBinary, MultiDiscrete
 import pygame
 
 import numpy as np
@@ -7,17 +7,17 @@ from scipy.spatial import distance_matrix
 
 
 class BaseWorld(gym.Env):
-    metadata = {"render_modes": ["human"], "render_fps": 3}
+    metadata = {"render_modes": ["human"], "render_fps": 60}
     def __init__(
         self,
         num_players: int = 1,
         num_pellets: int = 200,
         pellet_mass: float = 1.0,
         player_mass_base: float = 10.0,
-        player_mass_decay: float = 0.999,
+        player_mass_decay: float = 0.99,
         player_speed_inv_pow: float = -0.44,
         player_speed_scale: float = 10.0,
-        world_size: int = 500,
+        world_size: int = 800,
         sqrt_mass_to_radius: float = 1.0,
         render_mode: str = None,
     ):
@@ -34,7 +34,7 @@ class BaseWorld(gym.Env):
         self.sqrt_mass_to_radius = sqrt_mass_to_radius
 
         # noop, right, up, left, down for each player
-        self.action_space = spaces.MultiDiscrete(np.array([num_players, 5]))
+        self.action_space = MultiDiscrete([5] * num_players)
         self._action_to_direction = np.array(
             [
                 [0, 0],
@@ -46,15 +46,16 @@ class BaseWorld(gym.Env):
         )
 
         # players and pellet positions
-        self.observation_space = spaces.Dict(
+        self.observation_space = Dict(
             {
-                "player_masses": spaces.Box(
+                "player_masses": Box(
                     low=0, high=np.inf, shape=(num_players,), dtype=np.float64
                 ),
-                "player_locations": spaces.Box(
+                "player_is_alive": MultiBinary(num_players),
+                "player_locations": Box(
                     low=0, high=world_size, shape=(num_players, 2), dtype=np.float64
                 ),
-                "pellet_locations": spaces.Box(
+                "pellet_locations": Box(
                     low=0, high=world_size, shape=(num_pellets, 2), dtype=np.float64
                 ),
             }
@@ -67,12 +68,13 @@ class BaseWorld(gym.Env):
         self.clock = None
         self.window_size = world_size
 
-    def reset(self, seed=None):
-        super().reset(seed=seed)
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed, options=options)
 
         self._player_masses = np.full(
             self.num_players, self.player_mass_base, dtype=np.float64
         )
+        self._player_is_alive = np.full(self.num_players, True, dtype=np.bool)
         self._player_locations = (
             self.np_random.random((self.num_players, 2)) * self.world_size
         )
@@ -97,7 +99,8 @@ class BaseWorld(gym.Env):
         self._update_player_radii()
 
         prev_masses = self._player_masses.copy()
-        _, pellets_eaten = self._update_player_masses()
+        players_eaten, pellets_eaten = self._update_player_masses()
+        self._update_player_is_alive(players_eaten)
         self._update_pellet_locations(pellets_eaten)
 
         observation = self._get_observation()
@@ -112,7 +115,6 @@ class BaseWorld(gym.Env):
         return observation, reward, terminated, truncated, info
 
     def render(self):
-        # TODO @lijandrew implement this
         if self.window is None and self.render_mode == "human":
             pygame.init()
             pygame.display.init()
@@ -130,7 +132,7 @@ class BaseWorld(gym.Env):
             radius = self._player_radii[i]
             pygame.draw.circle(
                 canvas,
-                (255, 0, 0),
+                (0, 0, 255),
                 (x, self.world_size - y - 1),  # correct for inverted y
                 radius
             )
@@ -162,13 +164,12 @@ class BaseWorld(gym.Env):
             pygame.quit()
 
     def _update_player_locations(self, actions):
-        self._player_locations = np.clip(
-            self._player_locations
-            + self._action_to_direction[actions]
-            * np.power(self._player_masses, self.player_speed_inv_pow)
+        self._player_locations[self._player_is_alive] = np.clip(
+            self._player_locations[self._player_is_alive]
+            + self._action_to_direction[actions[self._player_is_alive]]
+            * np.power(self._player_masses[self._player_is_alive], self.player_speed_inv_pow)[:, np.newaxis]
             * self.player_speed_scale,
-            0,
-            self.world_size,
+            0, self.world_size,
         )
 
     def _update_player_radii(self):
@@ -184,51 +185,56 @@ class BaseWorld(gym.Env):
 
     def _update_player_masses(self):
         # add mass for eaten pellets and players
-        RR = np.add.outer(self._player_radii, self._player_radii)
         player_eats_player = (
-            (self._player_to_player_distances < RR)
+            (self._player_to_player_distances < self._player_radii[:, np.newaxis])
             & (self._player_radii[:, np.newaxis] > self._player_radii[np.newaxis, :])
         ).astype(np.float64)
         player_eats_pellet = (
-            (self._player_to_pellet_distances < self.pellet_radius)
+            (self._player_to_pellet_distances < self._player_radii[:, np.newaxis] + self.pellet_radius)
             & (self._player_radii[:, np.newaxis] > self.pellet_radius)
         ).astype(np.float64)
 
-        players_eaten = np.any(player_eats_player, axis=0)
-        pellets_eaten = np.any(player_eats_pellet, axis=0)
+        players_eaten = np.nonzero(np.any(player_eats_player, axis=0))[0]
+        pellets_eaten = np.nonzero(np.any(player_eats_pellet, axis=0))[0]
 
         # divide mass evenly among players that ate the same player or pellet
         player_eats_player[:, players_eaten] /= np.sum(
-            player_eats_player[:, players_eaten], axis=1, keepdims=True
+            player_eats_player[:, players_eaten], axis=0, keepdims=True
         )
         player_eats_pellet[:, pellets_eaten] /= np.sum(
-            player_eats_pellet[:, pellets_eaten], axis=1, keepdims=True
+            player_eats_pellet[:, pellets_eaten], axis=0, keepdims=True
         )
         self._player_masses += np.sum(player_eats_pellet * self.pellet_mass, axis=1)
         self._player_masses += np.sum(player_eats_player * self._player_masses, axis=1)
 
         # decay player mass
-        self._player_masses *= self.player_mass_decay
+        self._player_masses[self._player_is_alive] = np.maximum(
+            self._player_masses[self._player_is_alive] * self.player_mass_decay,
+            self.player_mass_base)
 
         # set eaten players to 0 mass
-        self._player_masses = np.where(players_eaten, 0, self._player_masses)
+        self._player_masses[players_eaten] = 0
 
         return players_eaten, pellets_eaten
+    
+    def _update_player_is_alive(self, players_eaten):
+        self._player_is_alive[players_eaten] = False
 
     def _update_pellet_locations(self, pellets_eaten):
         # respawn eaten pellets in random unoccupied locations
-        for i in np.where(pellets_eaten)[0]:
+        for i in pellets_eaten:
             while np.any(
                 np.linalg.norm(
                     self._pellet_locations[i] - self._player_locations, axis=-1
                 )
                 < self._player_radii
             ):
-                self._pellet_locations[i] = self.np_random.rand(2) * self.world_size
+                self._pellet_locations[i] = self.np_random.random(2) * self.world_size
 
     def _get_observation(self):
         return {
             "player_masses": self._player_masses,
+            "player_is_alive": self._player_is_alive,
             "player_locations": self._player_locations,
             "pellet_locations": self._pellet_locations,
         }
